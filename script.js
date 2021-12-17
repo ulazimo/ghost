dc968889169523b2891852e3b8c22eaf956acfa1
ulazimo <33716609+ulazimo@users.noreply.github.com>
2021-12-17 23:05:46 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function ani() {
+  document.getElementById('img').src = 'https://i.ibb.co/1JTQq2s/RESIZED-20210916-Steiner-Lab-Live-Work-TEN.png';
+}
+
+function rec() {
+  document.getElementById('img').src = 'https://i.ibb.co/T2f7crt/RESIZED-blurred-1.png';
+}
